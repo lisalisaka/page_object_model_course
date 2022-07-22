eb980ed52c27425fc556baf895217193acd3e7ca
lisalisaka <e.platonova@rogii.com>
2022-07-22 21:28:16 +0400

    Add xfail param

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
-@pytest.mark.parametrize('offer_number', [0,1,2,3,4,5,6,7,8,9])
+@pytest.mark.parametrize('offer_number', [0,1,2,3,4,5,6,pytest.param(7, marks=pytest.mark.xfail),8,9])
 
 def test_guest_can_add_product_to_basket(browser, offer_number):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_number}"
